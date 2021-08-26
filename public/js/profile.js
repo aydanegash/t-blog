7493065fa9ad9e74eacca6bfcde477060bf69146
aydanegash <73369360+aydanegash@users.noreply.github.com>
2021-08-26 13:42:22 -0500

    all files

---
+++ public/js/profile.js
@@ -0,0 +1,47 @@
+const newFormHandler = async (event) => {
+  event.preventDefault();
+
+  const title = document.querySelector('#post-title').value.trim();
+  const content = document.querySelector('#post-content').value.trim();
+  const date = document.querySelector('#post-date').value.trim();
+
+  if (title && content && date) {
+    const response = await fetch(`/api/post`, {
+      method: 'POST',
+      body: JSON.stringify({ title, content, date }),
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    });
+
+    if (response.ok) {
+      document.location.replace('/profile');
+    } else {
+      alert('Failed to create post');
+    }
+  }
+};
+
+const delButtonHandler = async (event) => {
+  if (event.target.hasAttribute('data-id')) {
+    const id = event.target.getAttribute('data-id');
+
+    const response = await fetch(`/api/post/${id}`, {
+      method: 'DELETE',
+    });
+
+    if (response.ok) {
+      document.location.replace('/profile');
+    } else {
+      alert('Failed to delete post');
+    }
+  }
+};
+
+document
+  .querySelector('.new-post-form')
+  .addEventListener('post-btn', newFormHandler);
+
+document
+  .querySelector('.post-list')
+  .addEventListener('delete-post-btn', delButtonHandler);
